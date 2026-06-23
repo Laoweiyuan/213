fcb1801559c154d5206dc183e9b3b4ce021f20ed
Laoda <1173343520@qq.com>
2026-06-23 22:53:35 +0800

    Update Test.java

---
+++ src/main/java/com/example/demo/Controller/Test.java
@@ -0,0 +1,10 @@
+package com.example.demo.Controller;
+
+public class Test {
+    public void test(){
+        String nice="hello world";
+        System.out.println(nice);
+        //实验
+        //实验
+    }
+}
